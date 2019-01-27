f7f9f0a25728604ab57e6158802b61ae21404e6e
Anmol Singh <anmolshl2@gmail.com>
2019-01-26 19:06:54 -0800

    a

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
 from flask import Flask
+import requests
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+api_key = "AIzaSyABlH--kuE8mhX_wdycHH8L1bDuP7nsgOs"
+placesURL = "https://maps.googleapis.com/maps/api/place/details/json?"
+
+@app.route("/test")
+def home():
+    a = {"string": "Server is up!"}
+    return json.dumps(a)
+
+@app.route("/gTest")
+def getx():
+    a = {"string": "Server is up!"}
+    parameters = {'fields': 'rating,geometry', 'key':api_key, 'placeid':"ChIJN1t_tDeuEmsRUsoyG83frY4"}
+    r = requests.get(url=placesURL, params=parameters)
+    data = r.json()
+    return json.dumps(data)
